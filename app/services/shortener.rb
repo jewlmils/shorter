fe51a2870f7e79959cbe1c0e99964f8fc62b0a61
jewlmils <st.jewelmilagrosa@gmail.com>
2024-07-17 23:31:47 +0800

    refactor: switch from Digest to SecureRandom for generating unique lookup codes

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -14,23 +14,16 @@ class Shortener
     end
 
     def lookup_code
-        # get lookup code
-        # check if it already exists
-        # break out of the loop if it does not exist
-        # if not, get another code
-
-        i = 0
         loop do
-            code = get_fresh_code(i)
+            code = get_fresh_code
             break code unless link_model.exists?(lookup_code: code)
-            i = i + 1
         end
     end
 
     private
 
-    def get_fresh_code(i)
-        Digest::SHA256.hexdigest(url)[i..(i + 6)]
+    def get_fresh_code
+        SecureRandom.uuid[0..(0 + 6)]
     end
     
 end
