93aaa55c61a17e29d0768ef5b031523b94bf55e4
jewlmils <st.jewelmilagrosa@gmail.com>
2024-07-18 01:21:43 +0800

    test: ensure a Link is invalid if the URL is not formatted properly

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe Link, type: :model do
         expect(link.valid?).to be(true)
     end
 
+    it "is invalid if the URL is not formatted properly" do
+        link = Link.new(
+            original_url: "asdf",
+            lookup_code: "1234567"
+        )
+        expect(link.valid?).to be(false)
+    end
+
     it "is invalid without a lookup code" do
         link = Link.new(
             original_url: "https://www.favoritewebsite.com/articles/how-to-cook"
