81b83a2194254d88906a19728f586cf2b900a0d3
jewlmils <st.jewelmilagrosa@gmail.com>
2024-07-17 21:40:53 +0800

    test: add model tests for Link validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_17_133019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string "lookup_code"
+    t.string "original_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
---
+++ spec/models/link_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Link, type: :model do
+    it "is valid with an original URL and lookup code" do
+        link = Link.new(
+            original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+            lookup_code: "1234567"
+        )
+        expect(link.valid?).to be(true)
+    end
+
+    it "is invalid without a lookup code" do
+        link = Link.new(
+            original_url: "https://www.favoritewebsite.com/articles/how-to-cook"
+        )
+        expect(link.valid?).to be(false)
+    end
+
+    it "is invalid without an original URL" do
+        link = Link.new(
+            lookup_code: "1234567"
+        )
+        expect(link.valid?).to be(false)
+    end
+
+    it "is invalid with a duplicate lookup code" do
+        link = Link.new(
+            original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+            lookup_code: "1234567"
+        )
+        link.save
+
+        link_2 = Link.new(
+            original_url: "https://www.favoritewebsite.com/articles/how-to-bake",
+            lookup_code: "1234567"
+        )
+        expect(link_2.valid?).to be(false)
+    end
+end
